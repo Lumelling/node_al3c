68d92bd3bc18e57d71e6b4b69b0deccc5f8ac210
lumelling <thibault.stordeur.pro@gmail.com>
2021-04-12 19:07:40 +0200

    bugfix

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 const projets = require('./projets');
+const participants = require('./participants');
 
 router.use('/projets', projets);
+router.use('/participants', participants);
 
 module.exports = router;
